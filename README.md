14723dcd9788867d7971ebf31ce8b283e772e32d
aligocmen <aligocmen@powercoders.org>
2022-10-24 14:44:17 +0200

    to-do group project

---
+++ README.md
@@ -0,0 +1,9 @@
+1. on the stage of mobile version to markup all elements
+2. Hide elements that we don't need in moblie
+3. 2 media - mobile and tablet/desktop
+4. in mobile the form 
+    - appears as a block when press the btn "add new task", 
+    - .... and hides when press the btn "save" 
+    - and the task appears in the list
+    - contains btns "save", "clear", "close" (to think about closing conditions)
+5. btns in the form
---
+++ project.js
@@ -0,0 +1,35 @@
+let Tasks = []; // this will store all tasks
+
+// this is a class for a task
+class Task{
+    constructor(title,description,dateToStart){
+        this.Title = title;
+        this.Description = description;
+        this.DateToStart = dateToStart;
+    }
+}
+
+function showList(){
+
+}
+function sortList(){
+
+}
+function deleteTask(){
+
+}
+function updateTask(){
+
+}
+function createNewTask(){
+
+}
+function filterDoneUndone(){
+
+}
+function filterbyDate(){
+
+}
+function hideShowForm(){ // this will show/hide the update/create form
+
+}
